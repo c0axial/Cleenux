ca1c7f4def5a1b9ef712bbde2f5046a6d1ec67d8
KID-X <58233243+kid-x@users.noreply.github.com>
2019-12-01 01:37:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 clear
 
+echo -e "Giving Permission to Install..."
 chmod +x cleenux
 
 cp cleenux /bin
+echo -e "Copying Files..."
 
 echo -e "Installation Complete..."
+echo -e
+echo -e "Now on, Simply use command "cleenux""
 
 sleep 1
-
 exit
