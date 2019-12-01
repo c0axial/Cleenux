4c2e98b8aa651f08abd201582079feb3b4847357
terminalforlife <terminalforlife@yahoo.com>
2019-12-01 22:16:43 +0000

    Correct installation location and improve code
    Installing something like this into `/bin/` would be _very_ bad
    practice. A more appropriate place would `/usr/bin/`, although another
    common one is, I think, `/opt/`. I've addressed that in this commit.
    
    I've added some color. Just makes it look nicer, but of course not
    everyone will or can see it or see it properly. Feel free to get rid of
    the color escape sequences. Using `tput` to set the colors is better
    practice (more portable), but I have issues with that personally, so I
    wasn't sure if other people would too.
    
    Used `Exec()` to just avoid code repetition, and to make the whole
    process cleaner and easier with which to work.
    
    Errors are now a thing, and it's now much more robust.
    
    The ownership and group are now appropriately set, given its target.
    
    Lastly, ensured correct mode settings, because it's very important this
    is set correctly when installing to a PATH location. 755 is the norm.

--- install.sh
+++ install.sh
@@ -1,15 +1,36 @@
-#!/bin/bash
-clear
+#!/bin/sh
 
-echo -e "Giving Permission to Install..."
-chmod +x cleenux
+Progrm='cleenux'
 
-cp cleenux /bin
-echo -e "Copying Files..."
+if [ $UID -eq 0 ]; then
+	printf "ERROR: Root permissions required for system-wide changes.\n" 1>&2
+	exit 1
+fi
 
-echo -e "Installation Complete..."
-echo -e
-echo -e "Now on, Simply use command: cleenux"
+Exec(){
+	printf -v MSG "%s..." "$1"
+	shift
 
-sleep 1
-exit
+	$@
+
+	Red='\033[1;31m'
+	Green='\033[1;32m'
+	Reset='\033[0m'
+
+	if [ $? -gt 0 ]; then
+		printf "$Red[ERR]$Reset \n"
+	else
+		printf "$Red[OK]$Reset \n"
+	fi
+}
+
+if [ -f $Progrm -a -r $Progrm ]; then
+	Exec "[1/3] Installing to '/usr/bin/cleenux'" cp "$Progrm" /usr/bin/
+	Exec "[2/3] Setting correct file modes" chmod 755 "/usr/bin/$Progrm"
+	Exec "[3/3] Correcting ownership" chown 0:0 "/usr/bin/$Progrm"
+	printf "Done!\n"
+else
+	printf "ERROR: File '$Progrm' missing or inaccessible.\n" 1>&2
+	exit 1
+fi
+
